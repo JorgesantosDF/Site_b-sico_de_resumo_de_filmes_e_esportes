70202bcffd97b14e1a8c49c059f45b15d99216d1
JorgeSantosDF <jorgefilho18@gmail.com>
2026-02-02 19:48:45 -0300

    Subindo os Arquivos

---
+++ aaa.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class lava : MonoBehaviourScript{
+
+    string nomeCena;
+    bool usarLimite = true;
+    float velocidade =  
+
+    void Start(){
+
+    }
+
+    void Update(){
+
+    }
+}
